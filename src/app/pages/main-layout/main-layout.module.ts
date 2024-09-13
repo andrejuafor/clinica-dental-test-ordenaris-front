46d23801a16287707253dde10f2b300100f723cd
andrejuafor <andrejuafor1999@gmail.com>
2024-09-13 15:43:17 -0600

    the what makes us component has been layout

--- src/app/pages/main-layout/main-layout.module.ts
+++ src/app/pages/main-layout/main-layout.module.ts
@@ -5,17 +5,21 @@ import { MainLayoutComponent } from './main-layout.component';
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
 import { HowToGetServiceComponent } from './features/how-to-get-service/how-to-get-service.component';
 import { AlwaysLoughComponent } from './features/always-lough/always-lough.component';
+import { WhatMakesUsComponent } from './features/what-makes-us/what-makes-us.component';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
 
 @NgModule({
   declarations: [
     WeProvideHightComponent,
     HowToGetServiceComponent,
     MainLayoutComponent,
-    AlwaysLoughComponent
+    AlwaysLoughComponent,
+    WhatMakesUsComponent
   ],
   imports: [
     CommonModule,
-    MainLayoutRoutingModule
+    MainLayoutRoutingModule,
+    SharedModule
   ]
 })
 export class MainLayoutModule { }

56158158861b1abe32f70ed9233b14bbbfe05747
andrejuafor <andrejuafor1999@gmail.com>
2024-09-13 16:02:00 -0600

    the let us brighten component has been layout

---
+++ src/app/pages/main-layout/features/let-us-brighten/let-us-brighten.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-let-us-brighten',
+  templateUrl: './let-us-brighten.component.html',
+  styleUrls: ['./let-us-brighten.component.scss']
+})
+export class LetUsBrightenComponent {
+
+}

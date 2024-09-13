66dd1c615396a64711a8a6e73b72044e75179d60
andrejuafor <andrejuafor1999@gmail.com>
2024-09-12 20:05:25 -0600

    feat: footer-n-navbar-finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/main-layout/main-layout.module').then((m) => m.MainLayoutModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
